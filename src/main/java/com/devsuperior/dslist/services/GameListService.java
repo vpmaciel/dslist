89c669f8c728fd900231adc709ac397fa97126ca
vpmaciel@gmail.com <vpmaciell@gmail.com>
2026-02-24 05:44:59 -0300

    Get lists

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
+
+
+@Service
+public class GameListService {
+
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll() {
+		List<GameList> result = gameListRepository.findAll();
+		List<GameListDTO> dto = result.stream().map(x->new GameListDTO(x)).toList();
+		return dto;
+	}
+}
